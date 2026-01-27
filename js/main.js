37ad30684ad5e29a0ac37dec84b1925fcb84545f
MuzammilAsif <Muzammilasif257@gmail.com>
2026-01-28 01:49:25 +0500

    Did few tweaks on main style added a new file and more animations

--- js/main.js
+++ js/main.js
@@ -16,23 +16,39 @@ gsap.from(".nav-links li", {
 });
 
 
+gsap.from(".hero-right", {
+    x: 600,
+    opacity: 0,
+    duration: 1.2,
+    delay: 0.6,
+    ease: "power4.out"
+});
+gsap.from(".hero-right video", {
+    scale: 1,
+    opacity: 0,
+    duration: 1.2,
+    delay: 1,
+    ease: "power4.out"
+});
+
+
 // About section animation
 
 const aboutTL = gsap.timeline({
     scrollTrigger: {
         trigger: "#about",
-        start: "top 80%",
+        start: "top 101%",
         toggleActions: "play none none none",
         markers: false // turn true if debugging
     }
 });
 
-aboutTL.from("#about", {
-    x: -300,
-    opacity: 0,
-    duration: 1,
-    ease: "power4.out"
-});
+// aboutTL.from("#about", {
+//     x: -300,
+//     opacity: 0,
+//     duration: 1,
+//     ease: "power4.out"
+// });
 
 aboutTL.from(".about-text p", {
     x: 60,
@@ -42,18 +58,10 @@ aboutTL.from(".about-text p", {
     ease: "power3.out"
 }, "-=0.4");
 
-gsap.from(".hero-right", {
-    x: 100,
-    opacity: 0,
-    duration: 1.2,
-    ease: "power4.out"
-});
-
-gsap.from("#about", {
+aboutTL.from("#about", {
     x: -1000,
     opacity: 0,
     duration: 1,
-    delay: 0.5,
     ease: "power4.out"
 })
 aboutTL.from(".about-image img", {
@@ -69,6 +77,27 @@ aboutTL.from(".about-text h1", {
     duration: 0.8,
     ease: "power3.out"
 }, "-=0.5");
+
+const aboutBtn = document.querySelector(".aboutBtn");
+
+aboutBtn.addEventListener("click", () => {
+    
+    document.body.style.overflow = "hidden";
+    aboutBtn.style.color = "transparent";
+
+    gsap.to(aboutBtn, {
+        scale: 1000,
+        opacity: 1,
+        duration: 2,
+        ease: "power3.inOut",
+    });
+    // After animation, redirect to about.html
+    setTimeout(() => {
+        window.location.href = "DetailedOverview.html";
+    }, 800); // Match the timeout with animation duration
+});
+
+
 
 // services section animation
 gsap.from("#services", {
@@ -100,7 +129,7 @@ gsap.from("#work", {
     },
     y: 200,
     opacity: 0,
-    duration: 1.3, 
+    duration: 1.3,
     delay: 0.4,
     ease: "power4.out"
 })
